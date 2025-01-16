56beeed18ce4f38ecc4c9ce3c6ce7b3f8e919cad
Yoga <yogas5381@gmail.com>
2025-01-16 13:58:11 +0700

    ci-cd use kubernates

--- src/features/ProductSlice.jsx
+++ src/features/ProductSlice.jsx
@@ -1,48 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
+  import { createSlice } from "@reduxjs/toolkit";
+  import axios from "axios";
 
-const BASE_API = `${import.meta.env.VITE_API_URL || "http://localhost:9000"}/products`;
+  const BASE_API = `${import.meta.env.VITE_API_URL || "http://backend:9000"}/products`;
 
-const initialState = {
-  products: [],
-  loading: false,
-  error: null,
-};
+  const initialState = {
+    products: [],
+    loading: false,
+    error: null,
+  };
 
-const productSlice = createSlice({
-  name: "products",
-  initialState,
-  reducers: {
-    fetchProductsStart(state) {
-      state.loading = true;
-      state.error = null;
+  const productSlice = createSlice({
+    name: "products",
+    initialState,
+    reducers: {
+      fetchProductsStart(state) {
+        state.loading = true;
+        state.error = null;
+      },
+      fetchProductsSuccess(state, action) {
+        state.products = action.payload;
+        state.loading = false;
+      },
+      fetchProductsFailure(state, action) {
+        state.error = action.payload;
+        state.loading = false;
+      },
     },
-    fetchProductsSuccess(state, action) {
-      state.products = action.payload;
-      state.loading = false;
-    },
-    fetchProductsFailure(state, action) {
-      state.error = action.payload;
-      state.loading = false;
-    },
-  },
-});
+  });
 
-export const {
-  fetchProductsStart,
-  fetchProductsSuccess,
-  fetchProductsFailure,
-} = productSlice.actions;
+  export const {
+    fetchProductsStart,
+    fetchProductsSuccess,
+    fetchProductsFailure,
+  } = productSlice.actions;
 
-// Thunk
-export const fetchProducts = () => async (dispatch) => {
-  dispatch(fetchProductsStart());
-  try {
-    const response = await axios.get(BASE_API);
-    dispatch(fetchProductsSuccess(response.data));
-  } catch (error) {
-    dispatch(fetchProductsFailure(error.message));
-  }
-};
+  // Thunk
+  export const fetchProducts = () => async (dispatch) => {
+    dispatch(fetchProductsStart());
+    try {
+      const response = await axios.get(BASE_API);
+      dispatch(fetchProductsSuccess(response.data));
+    } catch (error) {
+      dispatch(fetchProductsFailure(error.message));
+    }
+  };
 
-export default productSlice.reducer;
+  export default productSlice.reducer;
