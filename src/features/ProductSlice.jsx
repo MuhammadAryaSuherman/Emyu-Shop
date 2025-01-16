68205e844e3276227209c8c793a1c38c9119caf3
Yoga <yogas5381@gmail.com>
2025-01-16 18:04:36 +0700

    ci-cd use kubernates

--- src/features/ProductSlice.jsx
+++ src/features/ProductSlice.jsx
@@ -1,7 +1,7 @@
   import { createSlice } from "@reduxjs/toolkit";
   import axios from "axios";
 
-  const BASE_API = `${import.meta.env.VITE_API_URL || "http://backend:9000"}/products`;
+  const BASE_API = `${import.meta.env.VITE_API_URL || "http://backend:80"}/products`;
 
   const initialState = {
     products: [],
